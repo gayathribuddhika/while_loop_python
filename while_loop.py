e69b072975c522b69d047202d96db6fcec0544c9
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-10-11 16:04:28 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,25 @@
+x = 1
+while x < 10:
+    print(x)
+    x += 1
+
+print(" ")
+
+n = 10
+while n > 0:
+    print(n)
+    n -= 1
+
+i = 1
+
+while i <= 5:
+    print("Hii", end=" ")
+
+    j = 1
+    while j <= 4:
+        print("Hello", end=" ")
+        j = j + 1
+
+    i = i + 1
+    print()
+
